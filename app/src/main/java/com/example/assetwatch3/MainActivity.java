f2cd959b6358bd55892bc7cfec45befeb28ad805
ssyed <syed.sohilanjum@orbcomm.com>
2021-02-04 16:57:39 +0530

    test

--- app/src/main/java/com/example/assetwatch3/MainActivity.java
+++ app/src/main/java/com/example/assetwatch3/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.assetwatch3;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,34 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    BottomNavigationView bottomView = findViewById(R.id.bottom_navigation);
+    bottomView.setOnNavigationItemSelectedListener(navListener);
+    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
   }
+
+  private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+      new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+          Fragment selectedFragment = null;
+          switch (item.getItemId())
+          {
+            case R.id.nav_home:
+              selectedFragment = new HomeFragment();
+              break;
+            case R.id.nav_assets:
+              selectedFragment = new AssetsFragment();
+              break;
+            case R.id.nav_map:
+              selectedFragment = new MapFragment();
+              break;
+            case R.id.nav_settings:
+              selectedFragment = new SettingsFragment();
+              break;
+          }
+          getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+          return  true;
+        }
+      };
 }
